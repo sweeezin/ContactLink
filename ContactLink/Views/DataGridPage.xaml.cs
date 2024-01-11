fe39cf88db13494795647f7d39dd0ed4fff2f54c
Gurarman Dhanoa <Dhanoagurarman@gmail.com>
2024-01-11 10:59:40 -0800

    updated horizontal dropdown menu

--- ContactLink/Views/DataGridPage.xaml.cs
+++ ContactLink/Views/DataGridPage.xaml.cs
@@ -37,6 +37,8 @@ public partial class DataGridPage : Page
     private void Add(object sender, System.Windows.RoutedEventArgs e)
     {
 
+
+
     }
 
     private void DataGridDisplay_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
@@ -62,72 +64,28 @@ public partial class DataGridPage : Page
 
     }
 
-    private void SetRenderTransformOrigin(UIElement element, double x, double y)
-    {
-        if (element.RenderTransform is TransformGroup transformGroup)
-        {
-            // If there is a TransformGroup, add a new RotateTransform and set its CenterX and CenterY
-            var rotateTransform = new RotateTransform();
-            rotateTransform.CenterX = x;
-            rotateTransform.CenterY = y;
-
-            transformGroup.Children.Add(rotateTransform);
-        }
-        else
-        {
-            // If there is no TransformGroup, create a new one with a RotateTransform
-            var transformGroup2 = new TransformGroup();
-            var rotateTransform = new RotateTransform();
-            rotateTransform.CenterX = x;
-            rotateTransform.CenterY = y;
-
-            transformGroup2.Children.Add(rotateTransform);
-            element.RenderTransform = transformGroup2;
-        }
-    }
 
     private void DropdownMenuClicked(object sender, System.Windows.RoutedEventArgs e)
     {
 
-        MenuOpen = !MenuOpen;
+        AddButton.Visibility = System.Windows.Visibility.Visible;
+        DeleteButton.Visibility = System.Windows.Visibility.Visible;
+        OpenFrame.Visibility = System.Windows.Visibility.Visible;
+        ClosedFrame.Visibility = System.Windows.Visibility.Hidden;
+        HorizontalDropdown2.Visibility = System.Windows.Visibility.Visible;
+        HorizontalDropdown.Visibility = System.Windows.Visibility.Hidden;
 
-        if (MenuOpen)
-        {
-            System.Windows.Thickness buttonMargin = new System.Windows.Thickness(0, 0, 145, 29);
+    }
 
-            HorizontalDropdown.Margin = buttonMargin;
+    private void HorizontalDropdownClicked2(object sender, RoutedEventArgs e)
+    {
 
-            AddButton.Visibility = System.Windows.Visibility.Visible;
-            DeleteButton.Visibility = System.Windows.Visibility.Visible;
-            OpenFrame.Visibility = System.Windows.Visibility.Visible;
-            ClosedFrame.Visibility = System.Windows.Visibility.Hidden;
-
-            RotateTransform rotateTransform = new RotateTransform(180);
-
-            HorizontalDropdown.RenderTransform = rotateTransform;
-            SetRenderTransformOrigin(HorizontalDropdown, 0.5, 0.5);
-
-
-        }
-        else
-        {
-            System.Windows.Thickness buttonMargin = new System.Windows.Thickness(0, 0, 59, 29);
-
-            HorizontalDropdown.Margin = buttonMargin;
-
-            AddButton.Visibility = System.Windows.Visibility.Hidden;
-            DeleteButton.Visibility = System.Windows.Visibility.Hidden;
-            OpenFrame.Visibility = System.Windows.Visibility.Hidden;
-            ClosedFrame.Visibility = System.Windows.Visibility.Visible;
-
-            RotateTransform rotateTransform = new RotateTransform();
-            rotateTransform.Angle += 180;
-
-            HorizontalDropdown.RenderTransform = rotateTransform;
-
-            SetRenderTransformOrigin(HorizontalDropdown, 0.5, 0.5);
-
-        }
+        AddButton.Visibility = System.Windows.Visibility.Hidden;
+        DeleteButton.Visibility = System.Windows.Visibility.Hidden;
+        OpenFrame.Visibility = System.Windows.Visibility.Hidden;
+        ClosedFrame.Visibility = System.Windows.Visibility.Visible;
+        HorizontalDropdown2.Visibility = System.Windows.Visibility.Hidden;
+        HorizontalDropdown.Visibility = System.Windows.Visibility.Visible;
 
     }
 
